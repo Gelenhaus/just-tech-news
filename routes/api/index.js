3d94be555d430aaa9a8672abdd86ae420305c7d8
Gerrit Elenbaas <84500222+Gelenhaus@users.noreply.github.com>
2021-11-02 12:06:33 -0600

    back at it

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
-
 const apiRoutes = require('./user-routes');
 
 router.use('/api', apiRoutes);
 
 router.use((req, res) => {
-    res.status(404).end();
+    res.status(200);
+    res.send('Hello');
 });
 
 module.exports = router;
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -2,32 +2,36 @@ const router = require('express').Router();
 const { User } = require('../../models');
 
 // get all users
-User.findAll({
-    attributes: { exclude: ['password'] }
-})
-    .then(dbUserData => res.json(dbUserData))
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
-
-User.findOne({
-    attributes: { exclude: ['password'] },
-    where: {
-        id: req.params.id
-    }
-})
-    .then(dbUserData => {
-        if (!dbUserData) {
-            res.status(404).json({ message: 'No user found with this id' });
-            return;
-        }
-        res.json(dbUserData);
+router.get('/', (req, res) => {
+    User.findAll({
+        attributes: { exclude: ['password'] }
     })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.get('/:id', (req, res) => {
+    User.findOne({
+        attributes: { exclude: ['password'] },
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
 
 router.post('/', (req, res) => {
     // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}

4789a6c8c6f2f085573f0ab678c160dca43fbea3
Gerrit Elenbaas <84500222+Gelenhaus@users.noreply.github.com>
2021-11-06 17:47:03 -0600

    fingers crossed

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,12 @@ const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
 
+router.use("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"))
+});
+
+
 router.use((req, res) => {
     res.status(404).end();
 });
-
 module.exports = router;
